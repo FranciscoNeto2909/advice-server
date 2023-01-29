6042aad0939f324fead45ed1a00861200b44b57f
Francisco Neto <wantedshark@gmail.com>
2023-01-29 14:23:41 -0300

    title changed to author

--- src/db/advices.js
+++ src/db/advices.js
@@ -1,18 +1,15 @@
 const v4 = require("uuid")
 const uuid = v4.v4
 let db = {
-    suggestedAdvice:[
-        
-    ],
     advices:[
         {
             id:uuid(),
-            title:"Some advice",
+            author:"Unknown",
             advice:"Do what your heart tells you"
         },
         {
             id:uuid(),
-            title:"Some other advice",
+            author:"Unknown",
             advice:"Think before do"
         }
     ]
--- src/index.js
+++ src/index.js
@@ -10,18 +10,7 @@ app.listen(port, console.log("Executando..."))
 app.get("/", (req, res) => {
     res.send(db.advices)
 })
-app.get("/suggestedAdvice", (req, res) => {
-    res.send(db.suggestedAdvice)
-})
-app.post("/suggestAdvice", (req, res) => {
-    const advice = req.body
-    if (advice.title !== "" && advice.advice !== "") {
-        db.suggestedAdvice.push(advice)
-        res.send(advice)
-    } else {
-        console.error()
-    }
-})
+
 app.post("/addAdvice", (req, res) => {
     const advice = req.body
     if (advice.title !== "" && advice.advice !== "") {
@@ -31,11 +20,8 @@ app.post("/addAdvice", (req, res) => {
         console.error()
     }
 })
+
 app.delete("/removeAdvice/:id", (req, res) => {
     const adviceId = req.params.id
     db.advices = db.advices.filter(adv => adv.id.toString() !== adviceId.toString())
 })
-app.delete("/removeSuggestedAdvice/:id", (req, res) => {
-    const adviceId = req.params.id
-    db.suggestedAdvice = db.suggestedAdvice.filter(adv => adv.id.toString() !== adviceId.toString())
-}) 
